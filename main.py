dddd6eae2b1b826e19d2d25aa6b4dd5ac5f74470
Vedmedk0 <Vedmedk0@mail.ru>
2019-03-03 20:10:53 +0300

    hehe

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from objects import BarberShop, Barber, Customer
 from objects import customerIntervalMin, customerIntervalMax
 import time, random
 
+# hi Karina
+
 if __name__ == "__main__":
     customers = [Customer(0), Customer(1), Customer(2), Customer(3), Customer(4), Customer(5), ]
     barber = Barber()
